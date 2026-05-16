15c81f5ecd886a3488d9117fb61af31d04c14501
baodhg <dhgb2k5@gmail.com>
2026-05-16 21:16:43 +0700

    fix: suppress hydration warning on html tag for dynamic theme injection

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="vi" className={`${geistMono.variable} ${spaceGrotesk.variable} h-full antialiased`}>
+    <html lang="vi" className={`${geistMono.variable} ${spaceGrotesk.variable} h-full antialiased`} suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
       </head>
